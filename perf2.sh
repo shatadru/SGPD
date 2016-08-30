87dd34a3d16e971cc8a47889c7c911f33258e16b
Shatadru Bandyopadhyay <shatadru1@gmail.com>
2016-08-30 17:08:00 +0530

    Rename perf.sh to perf2.sh
    Compatibility for RHEL 5

--- perf2.sh
+++ perf2.sh
@@ -13,11 +13,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-
-
+lsbyes=1
+iotopyes=0
+iostatold=0
 function pkg_check(){
 which $1 > /dev/null 2> /dev/null
-
 if [ "$?" -ne "0" ];then
 	echo Command : $1 Not found...
 	if [ "$1" == "iotop" ]; then
@@ -26,6 +26,8 @@ if [ "$?" -ne "0" ];then
 		elif [ "$1" == "sar" ]; then 
 		echo "Install systat package (#yum install sysstat) and run the script again. exiting..."
 		exit
+		elif [ "$1" == "lsb_release" ]; then 
+		lsbyes=0
 		else
 		echo "Install $1 package (#yum install $1) and run the script again. exiting..."
 		exit
@@ -35,8 +37,26 @@ fi
 }
 
 pkg_check sar
-pkg_check iotop
+pkg_check lsb_release
 
+## OS CHECK ##
+
+if [ $lsbyes -eq "1" ]; then
+version=`lsb_release -r|cut -f2`
+else
+version=`cat /etc/redhat-release |cut -f7 -d " "`
+fi
+
+v=`echo $version|cut -f1 -d "."`
+
+if [ $v -ge "6" ];then
+pkg_check iotop
+iotopyes=1
+else
+echo "iotop command will not be collected as system is RHEL 5 or lower"
+iotopyes=0
+iostatold=1
+fi
 
 ITERATION=30
 INTERVAL=2  # default interval
@@ -74,7 +94,11 @@ trap end SIGHUP SIGINT SIGTERM
 
 #~~~ Continuous collection by will run outside loop ~~~
 date >> /tmp/vmstat.out; vmstat $INTERVAL $ITERATION >> /tmp/vmstat.out &
-iostat -t -z -x $INTERVAL $ITERATION >> /tmp/iostat.out &
+if [ "$iostatold" -eq "1" ]; then
+	iostat -t  -x $INTERVAL $ITERATION >> /tmp/iostat.out &
+	else
+	iostat -t -z -x $INTERVAL $ITERATION >> /tmp/iostat.out &
+fi
 sar $INTERVAL $ITERATION >> /tmp/sar.out &
 sar -A $INTERVAL $ITERATION -p >> /tmp/sarA.out &
 mpstat $INTERVAL $ITERATION -P ALL >> /tmp/mpstat.out &
@@ -89,7 +113,9 @@ do
 	then	
 		echo "$(date +%T): Collecting data : Iteration "$(($CURRENT_ITERATION))
 		date >> /tmp/top.out; top -n 1 -b >> /tmp/top.out
-		date >> /tmp/iotop.out; iotop -n 1 -b >> /tmp/iotop.out
+		if [ "$iotopyes" -eq "1" ]; then
+			date >> /tmp/iotop.out; iotop -n 1 -b >> /tmp/iotop.out
+		fi
 		date >> /tmp/mem.out; cat /proc/meminfo >> /tmp/mem.out
 		date >> /tmp/free.out; free -m >> /tmp/free.out
 		date >> /tmp/psf.out; ps auxf >> /tmp/psf.out

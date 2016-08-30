c97d110d319ea57867a8e3d7c51c3c5f473a0c79
Shatadru Bandyopadhyay <shatadru1@gmail.com>
2016-08-30 17:24:13 +0530

    Update perf.sh

--- perf.sh
+++ perf.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #  Author : Shatadru Bandyopadhyay
 #         : Ganesh Gore
+#
+# Licenced under GPLv3, check LICENSE.txt
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,

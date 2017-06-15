0f14c653eb4a324caefde5c03577cd1cb3f093a1
Shatadru Bandyopadhyay <shatadru1@gmail.com>
2017-06-15 09:28:26 +0530

    Create command_out.sh

--- command_out.sh
+++ command_out.sh
@@ -23,6 +23,10 @@ uname -a &> uname
 echo $host &> hostname
 
 # lvm 
+mkdir -p etc
+mkdir -p etc/lvm
+
+cat /etc/lvm/lvm.conf > etc/lvm/lvm.conf
 lvs -o +devices &> lvs
 vgs -o +devices &> vgs
 pvs  &> pvs
@@ -35,12 +39,14 @@ dmsetup table &> dmestup_table
 dmsetup info -c &> dmsetup_info_c
 
 # multipath -l
+cat /etc/multipath.conf > etc/multipath.conf
 multipath -l &> multipath_l
 
 # log 
 dmesg > dmesg
 tail -100 /var/log/messages > messages
 
+ls -laR /dev > ls_laR_dev
 
 tar -cf /tmp/"$FILENAME" $DIR 2>/dev/null
 
